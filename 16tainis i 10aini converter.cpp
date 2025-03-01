d271606173b581a2fc4b045529887e3fea752c94
Augustas Povilauskas <augustaspovil@gmail.com>
2025-03-01 12:41:39 +0200

    Update 16tainis i 10aini converter.cpp

--- 16tainis i 10aini converter.cpp
+++ 16tainis i 10aini converter.cpp
@@ -1,36 +1,33 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
+#include <bits/stdc++.h>
 
-int convert(char temp) {
-    if (temp >= 'A' && temp <= 'F') return temp - 'A' + 10;
-    return temp - '0';
+std::string konvert(int sk){
+    std::string ats[16] = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+    return ats[sk/16]+ats[sk%16];
+    
 }
 
-int main() {
-    int ilgis, plotis;
+
+int main()
+{
     std::ifstream fd("U1.txt");
     std::ofstream fr("U1rez.txt");
-
-    if (!fd.is_open() || !fr.is_open()) {
-        std::cerr << "Error opening file.\n";
-        return 1;
+    int a,b;
+    fd >> a >> b;
+    
+    for(int i = 0; i<a; i++){
+        for(int j=0; j<b; j++){
+            if(j!=0) fr <<';';
+            int sk;
+            fd >> sk;
+            fr << konvert(sk);
+            fd >> sk;
+            fr << konvert(sk);
+            fd >> sk;
+            fr <<konvert(sk);
+        }
+        fr << '\n';
     }
-
-    fd >> ilgis >> plotis;
-
-    std::vector<int> a;
-    for (int i = 0; i < ilgis * plotis * 3; i++) {
-        char temp, temp1;
-        fd >> temp >> temp1; // Read two characters (hexadecimal digits)
-        a.push_back(convert(temp) * 16 + convert(temp1));
-    }
-
-    for (int i = 0; i < a.size(); i++) {
-        fr << a[i];
-        if ((i + 1) % 3 == 0) fr << "; ";
-        else fr << ' ';
-    }
+    
 
     return 0;
 }
